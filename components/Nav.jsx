cbd65c7a8d80f406faf04172e70d6a868a486b06
tayoolaigbe <tayoolaigbe@outlook.com>
2023-08-27 16:21:01 +0100

    Completed the Navbar UI

---
+++ components/Nav.jsx
@@ -0,0 +1,126 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+
+const Nav = () => {
+	const isUserLoggedIn = true;
+	const [providers, setProviders] = useState(null);
+	const [toggleDropDown, setToggleDropDown] = useState(false);
+
+	useEffect(() => {
+		const setProviders = async () => {
+			const response = await getProviders();
+
+			setProviders(response);
+		};
+		setProviders();
+	}, []);
+
+	return (
+		<nav className="flex-between w-full mb-16 pt-3">
+			<Link href="/" className="flex gap-2 flex-center">
+				<Image
+					src="/assets/images/logo.svg"
+					alt="Promptopia Logo"
+					width={30}
+					height={30}
+					className="object-contain"
+				/>
+				<p className="logo_text">Promptopia</p>
+			</Link>
+			{/* Desktop Navigation */}
+			<div className="sm:flex hidden">
+				{isUserLoggedIn ? (
+					<div className="flex gap-3 md:gap-5">
+						<Link href="/create-prompt" className="black_btn">
+							Create Post
+						</Link>
+						<button type="button" className="outline_btn" onClick={signOut}>
+							Sign Out
+						</button>
+						<Link href="/profile">
+							<Image
+								src="/assets/images/logo.svg"
+								width={37}
+								height={37}
+								alt="profile"
+								className="rounded-full"
+							/>
+						</Link>
+					</div>
+				) : (
+					<>
+						{providers &&
+							Object.values(providers).map(provider => (
+								<button
+									type="button"
+									kep={provider.name}
+									onClick={() => signIn(provider.id)}
+								></button>
+							))}
+					</>
+				)}
+			</div>
+
+			{/* Mobile Navigation */}
+			<div className="sm:hidden flex relative">
+				{isUserLoggedIn ? (
+					<div className="flex">
+						<Image
+							src="/assets/images/logo.svg"
+							width={37}
+							height={37}
+							alt="profile"
+							className="rounded-full"
+							onClick={() => setToggleDropDown(prev => !prev)}
+						/>
+						{toggleDropDown && (
+							<div className="dropdown">
+								<Link
+									href="/profile"
+									className="dropdown_link"
+									onClick={() => setToggleDropDown(false)}
+								>
+									My Profile
+								</Link>
+								<Link
+									href="/create-prompt"
+									className="dropdown_link"
+									onClick={() => setToggleDropDown(false)}
+								>
+									Create Prompt
+								</Link>
+								<button
+									type="button"
+									onClick={() => {
+										setToggleDropDown(false);
+										signOut();
+									}}
+									className="mt-5 w-full black_btn"
+								>
+									Sign Out
+								</button>
+							</div>
+						)}
+					</div>
+				) : (
+					<>
+						{providers &&
+							Object.values(providers).map(provider => (
+								<button
+									type="button"
+									kep={provider.name}
+									onClick={() => signIn(provider.id)}
+								></button>
+							))}
+					</>
+				)}
+			</div>
+		</nav>
+	);
+};
+
+export default Nav;
